21d93a73393d8794de84aebcd56c1583b7948d39
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 <sefikaefeoglu@gmail.com>
2023-06-17 00:30:57 +0200

    add script

---
+++ scripts/data_collection.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=data_wiki            
+#SBATCH --mail-user=sefie08@zedat.fu-berlin.de  
+#SBATCH --mail-type=end
+#SBATCH --nodes=1
+#SBATCH --ntasks=1                        
+#SBATCH --mem-per-cpu=10240                  
+#SBATCH --time=14:00:00                           
+#SBATCH --qos=standard                         
+#SBATCH --cpus-per-task=1
+
+
+cd /home/${USER}/projects/
+
+module add Python/3.9.5-GCCcore-10.3.0
+
+source ~/path/to/new/virtual/environment/bin/activate
+
+python wiki_query_service.py
