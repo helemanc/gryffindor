5b4ccbd35c89334c639652c09d38733dbb077f3e
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 <sefikaefeoglu@gmail.com>
2023-06-22 11:16:17 +0200

    fix repo

--- src/data-preprocessing/get_dbpedia_abstracts.py
+++ src/data-preprocessing/get_dbpedia_abstracts.py
@@ -1,16 +1,28 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 """
 Created on Wed Jun 21 12:07:18 2023
 
 @author: Celian
 """
 
-import json
 from SPARQLWrapper import SPARQLWrapper, JSON
-import requests
 import time 
+import configparser
+import os
+import sys
+sys.path.append('../')
+
+PACKAGE_PARENT = '.'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+PREFIX_PATH = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:-2]) + "/"
+
+from utils import read_data, write_data
 
 def get_results(query):
+    """ Get the result from the sparql query"""
+
     sparql = SPARQLWrapper("http://dbpedia.org/sparql")
     sparql.setReturnFormat(JSON)
 
@@ -18,42 +30,62 @@ def get_results(query):
 
     return sparql.query().convert()
 
+def get_query(Qid):
+    """ Get the query to get the abstract of an item"""
 
-def get_Abstract(Qid):
-    wikipedia_abstracts={}    
-    #Qid="http://www.wikidata.org/entity/Q19844547"
-    #Qid=item['item info']['item_id']
-    query="""SELECT ?s ?abstract  where {
+    query = """SELECT ?s ?abstract  where {
     ?s dbo:abstract ?abstract.
     ?s owl:sameAs <"""+Qid+""">.
     FILTER (lang(?abstract) = 'en')
     }
     LIMIT 1
     """
-    query=query.replace("\n","").strip()
-    e=get_results(query)
+    return query
+
+def get_Abstract(Qid):
+    """ Get the abstract of an item
+    args: 
+        Qid: the id of the item
+        return the abstract of the item or False if the query is empty
+        """  
+    #Qid="http://www.wikidata.org/entity/Q19844547"
+    #Qid=item['item info']['item_id']
+
+    query = get_query(Qid)
+
+    query = query.replace("\n","").strip()
+    e = get_results(query)
+
     print(query)
-    abs_=""
+
+    abs_ = ""
+
     for res in e["results"]["bindings"]:
-        if res["abstract"]["value"]!="":
-            abs_=res["abstract"]["value"]
+        if res["abstract"]["value"] != "":
+            abs_ = res["abstract"]["value"]
+
     return abs_
 
-if __name__ == '__main__':            
-    file0="C:/Users/Celian/Downloads/query.json"
-    with open(file0) as user_file:
-      parsed_json = json.load(user_file)
+if __name__ == '__main__':  
+
+    print(PREFIX_PATH)
+
+    config = configparser.ConfigParser()
+    config.read(PREFIX_PATH + "config.ini")
+    query_file = PREFIX_PATH + config["DATA"]["wikidata_query_file"]
+    parsed_json = read_data.read_json(query_file)
     
-    all_len=len(parsed_json)
-    dict_id_abs={}
-    
+    all_len = len(parsed_json)
+    dict_id_abs = {}
+
     for i in range(len(parsed_json)):
         print("GET ABSTRACT >",i,"/",all_len)
-        Qid=parsed_json[i]["item"]
+        Qid = parsed_json[i]["item"]
+
         if(Qid not in dict_id_abs.keys()):
-            dict_id_abs[Qid]=get_Abstract(Qid)
+            dict_id_abs[Qid] = get_Abstract(Qid)
             time.sleep(1)
     
-    with open('C:/Users/Celian/Downloads/dbpedia_abstracts_5159sample_withIMG.json', 'w', encoding='utf-8') as f:
-        json.dump(dict_id_abs, f, ensure_ascii=False, indent=4)
+    # use utils to save the dict.
+    write_data.save_data(PREFIX_PATH + config["DATA"]["dbpedia_abstracts"], dict_id_abs)
         
---
+++ src/data-preprocessing/ground_truth_image_downloader.py
@@ -0,0 +1,59 @@
+import os
+import sys
+import urllib.request
+import requests
+import urllib
+sys.path.append('../')
+import configparser
+
+PACKAGE_PARENT = '.'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+PREFIX_PATH = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:-2]) + "/"
+
+from utils import read_data, write_data
+
+
+def main(raw_data_path, ground_truth_path_images, unavailable_pic_ids):
+    """ Download the images from the ground truth
+    Args:
+        raw_data_path (str): path to the raw data
+        ground_truth_path_images (str): path to the ground truth images
+        unavailable_pic_ids (str): path to the unavailable pic ids
+    """
+    data = read_data.read_json(raw_data_path)
+    images_id = []
+    for i in range(0, len(data)):
+        # print(data[i]["item info"]['pic'])
+        try:
+            if data[i]["item info"]['pic'] != None:
+                path = ground_truth_path_images + str(data[i]["item info"]['item_id']).split("/")[-1]+"_"+str(data[i]["item info"]['label'])
+                os.mkdir(path)
+
+                urllib.request.urlretrieve(data[i]["item info"]['pic'], path +"/"+ str(data[i]["item info"]['item_id']).split("/")[-1]+"_"+str(data[i]["item info"]['label'])+"."+data[i]["item info"]['pic'].split("/")[-1].split(".")[-1])
+        except:
+            # print(data[i]["item info"]['item_id'])
+            try:
+                page = requests.get(data[i]["item info"]['pic'])
+                img_url = page.url
+                urllib.request.urlretrieve(img_url, path + "/" + str(data[i]["item info"]['item_id']).split("/")[-1]+"_"+str(data[i]["item info"]['label'])+".jpg")
+            except:
+                # print(img_url)
+                images_id.append({"item_id": data[i]["item info"]['item_id'], "pic":data[i]["item info"]['pic']})
+    
+    write_data.save_data(unavailable_pic_ids, images_id)
+
+    
+    
+if __name__ == "__main__":
+
+    print(PREFIX_PATH)
+
+    config = configparser.ConfigParser()
+    config.read(PREFIX_PATH + "config.ini")
+    raw_data_path = config["PATH"]["RAW_DATA_PATH"]
+    ground_truth_path_images = config["PATH"]["GROUND_TRUTH_PATH_IMAGES"]
+    unavailable_pic_ids = config["PATH"]["UNAVAILABLE_PIC_IDS"]
+
+    main(raw_data_path, ground_truth_path_images, unavailable_pic_ids)
+
