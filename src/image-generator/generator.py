e0ce28099015e67e1504b6cfbbfba6e235c28404
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 <sefikaefeoglu@gmail.com>
2023-06-22 11:18:12 +0200

    remove hard code generator

--- src/image-generator/generator.py
+++ src/image-generator/generator.py
@@ -1,12 +1,20 @@
 import json
 import openai
 import os
+import sys
+import configparser
 import urllib.request
 
-def read_data(file_name):
-    with open(file_name, 'r') as f:
-        data = json.load(f)
-    return data
+sys.path.append('../')
+
+PACKAGE_PARENT = '.'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+PREFIX_PATH = "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:-2]) + "/"
+
+
+
+from utils import read_data
 
 def load_api_key(secrets_file="secrets.json"):
     with open(secrets_file) as f:
@@ -24,48 +32,54 @@ def image_generator(prompt):
     except:
         return None
 
+def generate_images(item_list, generated_path, original_path):
 
-api_key = load_api_key()
-openai.api_key = api_key
-item_list = read_data("wiki_fiction_data_image_withabstract.json")
-generated_images = []
-
-for i, item in enumerate(item_list):
-
-    if i>-1:
+    for i, item in enumerate(item_list):
         # print(item)
-        path = "../data/"+str(item["item_id"].split("/")[-1])+"/"
+        path = generated_path + str(item["item_id"].split("/")[-1]) + "/"
 
         print(os.path.isdir(path))
+
         if os.path.isdir(path):
             os.makedirs(path)
-            verbalised_prompt = " ".join([char for char in item["verbalised_prompt"].split(" "))
-            wiki_abstract =  " ".join([char for char in item["wiki_abstract"].split(" "))
-            plain_prompt =  " ".join([char for char in item["plain_prompt"].split(" "))
+            verbalised_prompt = " ".join([char for char in item["verbalised_prompt"].split(" ")])
+            wiki_abstract =  " ".join([char for char in item["wiki_abstract"].split(" ")])
+            plain_prompt =  " ".join([char for char in item["plain_prompt"].split(" ")])
 
             row = {"basic_prompt_image": image_generator(item["basic_prompt"]),
                 "plain_prompt_image": image_generator(plain_prompt),
                 "verbalised_prompt_image": image_generator(verbalised_prompt),
                 "wiki_abstract_image": image_generator(wiki_abstract),
                 "ground_truth_image": item["pic"]}
+            
             if row['ground_truth_image'] != None:
-                urllib.request.urlretrieve(row['ground_truth_image'], original_path+str(item["item_id"].split("/")[-1])+"."+row['ground_truth_image'].split(".")[-1])
+                urllib.request.urlretrieve(row['ground_truth_image'], original_path + str(item["item_id"].split("/")[-1])+"."+row['ground_truth_image'].split(".")[-1])
             if row['basic_prompt_image'] != None:
                 urllib.request.urlretrieve(row['basic_prompt_image'], generated_path + 'basic_prompt.jpg')
             if row['plain_prompt_image'] != None:
                 urllib.request.urlretrieve(row['plain_prompt_image'], generated_path + 'plain_prompt.jpg')
             if row['verbalised_prompt_image'] != None:
                 urllib.request.urlretrieve(row['verbalised_prompt_image'], generated_path + 'verbalised_prompt.jpg')
-            if image != None:
-                urllib.request.urlretrieve(image, generated_path + 'wiki_abstract_prompt.jpg')
+            if row['wiki_abstract_image'] != None:
+                urllib.request.urlretrieve(row['wiki_abstract_image'], generated_path + 'wiki_abstract_prompt.jpg')
+
+def main(raw_data_path, generated_path, original_path):
+
+    api_key = load_api_key()
+    openai.api_key = api_key
+    item_list = read_data.read_json(raw_data_path)
+    generate_images(item_list, generated_path, original_path)
+
+
+if __name__ == '__main__':
+    
+    print(PREFIX_PATH)
+
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    generated_path = config['DATA']['generated_data_path']
+    original_path = config['DATA']['ground_truth_path']
+    raw_data_path = config['DATA']['data_with_images_path']
+
+    main(raw_data_path, generated_path, original_path)
             
-
-
-            
-
-
-
-
-
-
-
