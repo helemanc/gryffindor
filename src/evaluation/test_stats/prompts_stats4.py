a48e8248f49e69bed032f2305c84e30b5e15d71d
c_ringwald <42188996+datalogism@users.noreply.github.com>
2023-07-19 09:19:46 +0200

    StudentTest_relation_triples_based_prompts.py

--- src/evaluation/test_stats/prompts_stats4.py
+++ src/evaluation/test_stats/prompts_stats4.py
@@ -13,19 +13,17 @@ from bioinfokit.analys import stat
 from scipy import stats as st
 import numpy as np
 
+####### LOAD METRICS FILES
 stats_dict="C:/Users/Celian/Desktop/ISWS/SESSION2/data_others/Stats_by_entity(1).json"
-
 with open(stats_dict, encoding="utf8") as user_file:
   entity_stats = json.load(user_file)
-
-
 file="C:/Users/Celian/Downloads/all_metrics.json"
 with open(file, encoding="utf8") as user_file:
       metrics = json.load(user_file)
       
       
-      
-new_data=[]       
+
+##### COUNT RELATIONS
 relations={}
 for k in entity_stats["withIMG"].keys():
      QID=k.replace("http://www.wikidata.org/entity/","")
@@ -35,13 +33,13 @@ for k in entity_stats["withIMG"].keys():
              relations[rel]=0
          relations[rel]+=1
     
-# change sense
+# FILTER THEM 
+# we keep only relation used more than 100 times
 sorted_rel = sorted(relations.items(), key=lambda x:x[1],reverse=True)
 filtered_rel=[t[0] for t in sorted_rel if t[1]>=100]
 #filtered_rel=[t[0] for t in sorted_rel] 
 
-
-
+##### CREATE A FIRST dict listing all the entity by relations used
 rel_QID={}
 for rel in filtered_rel:
     print(rel)
@@ -54,8 +52,9 @@ for rel in filtered_rel:
         if(rel in rel_nb.keys() and QID not in rel_QID[rel]):
             rel_QID[rel].append(QID)
              
-
-########## FOR TRIPLES FIRST
+############ CREATE A SECOND DICT 
+# we for each relation we list entities having them key "1" 
+# and entities that doesn't used them key "0" 
 rel_subset_QID={}
 for QID in metrics.keys():
     #print(QID)
@@ -68,10 +67,15 @@ for QID in metrics.keys():
             else:
                 rel_subset_QID[r]["0"].append(QID)
 
+######### SAMPLING FOR BEING ABLE TO TEST
+# as it is easier to compare sample with same size 
+# for each relation we count for each case (1/0) 
+# the number of entities. Then we adjust both sample
+# to the minimum length of each case (1/0), by sampling
+# randomly them. 
 import random
 rel_subset_samples={}
 for rel in rel_subset_QID.keys():
-    
     if(rel not in rel_subset_samples.keys()):
         rel_subset_samples[rel]={"0":[],"1":[]}  
         
@@ -81,26 +85,27 @@ for rel in rel_subset_QID.keys():
     rel_subset_samples[rel]["1"]=random.sample(rel_subset_QID[rel]["1"], min_len)
     rel_subset_samples[rel]["0"]=random.sample(rel_subset_QID[rel]["0"], min_len)
 
-
-################## PLAIN PROMPT
+#########################" FOR PLAIN PROMPT
 SELECTED_prompt="plain_prompt"
-#"verbalised_prompt"
-#plain_prompt
+
+##ATTACH SCORE TO EACH ENTITIES
 rel_samples_scores={}
 for rel in rel_subset_samples.keys():
     rel_samples_scores[rel]=[]  
     for type_ in rel_subset_samples[rel].keys():
         for QID in rel_subset_samples[rel][type_]:
-            
             tempo={"type":type_,"clip":metrics[QID]["clip"][SELECTED_prompt]}
             rel_samples_scores[rel].append(tempo)
 
+########### STUDENT TEST FOR COMPARING MEAN OF BOTH SAMPLE
 list_of_signif_rel=[]
 results_all1=[]
+## create table of results
 for relation in rel_samples_scores.keys():
     df=pd.DataFrame.from_dict(rel_samples_scores[relation])
     #Two sample t-test (unpaired or independent t-test)
     res = stat()
+   # if variance are not equals the test fail > do not have to test it implictly we just keep tests that are working
     try:
         res.ttest(df=df, xfac="type", res="clip", test_type=2)
         n=df.shape[0]/2
@@ -125,7 +130,9 @@ for relation in rel_samples_scores.keys():
 
 df_triples=pd.DataFrame.from_dict(results_all1)
 
-################## VERBALIZED PROMPT
+
+#########################" FOR VERBALIZED PROMPT
+## same process
 SELECTED_prompt="verbalised_prompt"
 rel_samples_scores={}
 for rel in rel_subset_samples.keys():
@@ -163,8 +170,6 @@ for relation in rel_samples_scores.keys():
         results_all2.append(temp)
     except:
         print("PB with relation")
-
-
 
 df_verba=pd.DataFrame.from_dict(results_all2)
         
