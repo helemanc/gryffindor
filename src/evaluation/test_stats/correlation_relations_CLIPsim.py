cc601606673e5780b21699f0700d4972be74edf1
c_ringwald <42188996+datalogism@users.noreply.github.com>
2023-07-19 09:07:58 +0200

    Update and rename prompts_stats3.py to correlation_relations_CLIPsim.py

--- src/evaluation/test_stats/correlation_relations_CLIPsim.py
+++ src/evaluation/test_stats/correlation_relations_CLIPsim.py
@@ -9,16 +9,16 @@ import pandas as pd
 import seaborn as sns
 from scipy.stats import pearsonr
 
+#### load stats by entity file
 stats_dict="C:/Users/Celian/Desktop/ISWS/SESSION2/data_others/Stats_by_entity(1).json"
-
 with open(stats_dict, encoding="utf8") as user_file:
   entity_stats = json.load(user_file)
-
-
+### and aggregated metric file
 file="C:/Users/Celian/Downloads/all_metrics.json"
 with open(file, encoding="utf8") as user_file:
       metrics = json.load(user_file)
-        
+
+#### BUILD A dataframe
 new_data=[]       
 for k in entity_stats["withIMG"].keys():
      QID=k.replace("http://www.wikidata.org/entity/","")
@@ -35,10 +35,12 @@ for k in entity_stats["withIMG"].keys():
 
 df=pd.DataFrame.from_dict(new_data)
 
+######## PAIR PLOT for the analysis of the possible correlation
 g = sns.PairGrid(df)
 g.map_diag(sns.histplot)
 g.map_offdiag(sns.scatterplot)
 
+###### CORRELATION COMPUTATION
 print("TRIPLES - NB REL CORR")
 corr, _ = pearsonr(df["triples"], df["nb_rel"])
 print(corr)
@@ -49,7 +51,6 @@ print(corr)
 print("VERBA - NB REL CORR")
 corr, _ = pearsonr(df["verbalized"], df["nb_rel"])
 print(corr)
-
 print("VERBA - NB REL UNIQ CORR")
 corr, _ = pearsonr(df["verbalized"], df["nb_uniq_rel"])
 print(corr)
